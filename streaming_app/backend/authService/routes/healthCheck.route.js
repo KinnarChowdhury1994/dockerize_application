03543c6453cd97328a13132cf2f70249e4d4ae17
kincho94 <kinnar.chowdhury@msqube.com>
2024-07-14 20:16:09 +0530

    updated compose && dockerfiles for not compatibility

--- streaming_app/backend/authService/routes/healthCheck.route.js
+++ streaming_app/backend/authService/routes/healthCheck.route.js
@@ -3,7 +3,7 @@ const routes = express.Router()
 
 const healthCheckController = require('../controllers/healthCheck.controller')
 
-routes.get('/health/', healthCheckController.healthCheck)
+routes.get('/', healthCheckController.healthCheck)
 
 
 module.exports = routes
