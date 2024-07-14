90b4ee2a07fc7c2c950c76102edfe65b3e303429
kincho94 <kinnar.chowdhury@msqube.com>
2024-07-14 06:10:22 +0530

    modified

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ab1a8cd4235090e75b030d42387e5d6a6d4090d979087ab233c3f85a13e77cc
-size 3137
+oid sha256:63512095ee400b784d0c51ae04838a0d98b6bbe9c03e2598d3468bb55dd268d2
+size 3143
